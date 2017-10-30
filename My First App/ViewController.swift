14c1189599711d0de7e1ce4a4ed104282d7049c9
Akram Helil <akramhelil@akrams-macbook-pro.local>
2017-10-30 16:56:31 -0400

    Initial Commit

---
+++ My First App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My First App
+//
+//  Created by Akram Helil on 10/30/17.
+//  Copyright © 2017 Akram Helil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
